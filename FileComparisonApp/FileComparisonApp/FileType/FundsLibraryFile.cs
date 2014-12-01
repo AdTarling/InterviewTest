0fae7e9ad3300aef783ac13dae231048608dec77
Adam Tarling <adismad@hotmail.com>
2014-12-01 18:54:57 +0000

    #story/file-comparison
    - working console app solution

---
+++ FileComparisonApp/FileComparisonApp/FileType/FundsLibraryFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FileComparisonApp.FileType
+{
+    public class FundsLibraryFile
+    {
+        private string _fileString { get; set; }
+
+        public FundsLibraryFile(string filePath)
+        {
+            if(File.Exists(filePath)){
+                CreateFileString(filePath);
+            }
+        }
+
+        private void CreateFileString(string filePath)
+        {
+            var fileLineStringArray = File.ReadAllLines(filePath);
+
+            _fileString = string.Join("", fileLineStringArray);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var fundsLibraryFileIncoming = obj as FundsLibraryFile;
+            if ((object)fundsLibraryFileIncoming == null)
+            {
+                return false;
+            }
+
+            if (fundsLibraryFileIncoming.ContainsEqualityRegex() && this.ContainsEqualityRegex())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool ContainsEqualityRegex()
+        {
+            var equalityRegex = new Regex(@"ID \[<.*>\]");
+            return equalityRegex.IsMatch(_fileString);
+        }
+    }
+}
---
+++ FileComparisonApp/FileComparisonApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using FileComparisonApp.FileType;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileComparisonApp
+{
+    public class Program
+    {
+        private static List<FundsLibraryFile> _fundsLibraryFiles;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the FundsLibrary File Comparison App");
+
+            _fundsLibraryFiles = new List<FundsLibraryFile>();
+
+            ReadFileInput(1);
+            ReadFileInput(2);
+
+            if (_fundsLibraryFiles[0].Equals(_fundsLibraryFiles[1]))
+            {
+                Console.WriteLine("Files are equal");
+            }
+            else
+            {
+                Console.WriteLine("Files are not equal");
+            }
+
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+
+        private static void ReadFileInput(int fileNumber)
+        {
+            Console.WriteLine(string.Format("Please enter location of file {0} to compare:", fileNumber));
+            var filePath1 = Console.ReadLine();
+            if (File.Exists(filePath1))
+            {
+                var fundsLibraryFile = new FundsLibraryFile(filePath1);
+                _fundsLibraryFiles.Add(fundsLibraryFile);
+                Console.WriteLine("Processed");
+            }
+            else
+            {
+                Console.WriteLine("No file found at specified location.");
+                ReadFileInput(fileNumber);
+            }
+        }
+    }
+}
